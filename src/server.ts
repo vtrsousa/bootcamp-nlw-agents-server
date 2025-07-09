cd17da4b0b8b88e9bbd119539eeda6c544f18373
joaosousa <joao.sousa@corebiz.ag>
2025-07-08 22:46:47 -0300

    feat: add rooms listening

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { getRoomsRoute } from './db/http/routes/get-rooms.ts';
+import { env } from './env.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173',
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', () => {
+  return 'OK';
+});
+
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT });
