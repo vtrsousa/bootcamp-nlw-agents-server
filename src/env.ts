a296ae81152a4a794605043c18920393ba575b4e
joaosousa <joao.sousa@corebiz.ag>
2025-07-09 14:32:55 -0300

    feat: change port

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  PORT: z.coerce.number().default(3434),
+  PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url().startsWith('postgresql://')
 })
 
